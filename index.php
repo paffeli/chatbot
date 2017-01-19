0a8ab3c4cd40e3d183b12f9c81329e4be688bee4
paffeli <lukas.forsberg@gmail.com>
2017-01-19 12:47:02 +0200

    basic page for asking the name
    inserted a question box to ask the persons name. Added bootstrap and a
    own css

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+		<!-- css and bootstrap links -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	<title>welcome to bot-chan</title>
+</head>
+<body>
+<form>
+	<label for="name">your name</label>
+	<input type="text" name="name" id="name">
+	<input type="submit" value="Send">
+</form>
+</body>
+</html>
